b6bfc1f1175b6687e5fd3782def39d9dac422286
Afowowe Olumide Victor <volumide42@gmail.com>
2022-09-27 13:06:07 +0100

    download image implementation

--- src/controllers/profile/download.js
+++ src/controllers/profile/download.js
@@ -6,20 +6,20 @@ import path from "path";
 import Download from "../../models/Download.js";
 import Upload from "../../models/Upload.js";
 
-const download = (url) => {
-	const filename = path.basename(url);
-	return client.get(url, (res) => {
-		const fileStream = fs.createWriteStream(filename);
-		res.pipe(fileStream);
-		fileStream.on("error", (err) => {
-			console.log(err);
-		});
-		fileStream.on("finish", () => {
-			fileStream.close();
-		});
-		fileStream.on("close", () => console.log("stream closed"));
-	});
-};
+// const download = (url) => {
+// 	const filename = path.basename(url);
+// 	return client.get(url, (res) => {
+// 		const fileStream = fs.createWriteStream(filename);
+// 		res.pipe(fileStream);
+// 		fileStream.on("error", (err) => {
+// 			console.log(err);
+// 		});
+// 		fileStream.on("finish", () => {
+// 			fileStream.close();
+// 		});
+// 		fileStream.on("close", () => console.log("stream closed"));
+// 	});
+// };
 
 const downloadImage = (req, res) => {
 	try {
@@ -48,6 +48,23 @@ export const recordDownload = async (res, req) => {
 		console.log(error);
 		return res.status(500).json({ "message": error });
 	}
+};
+
+export const bookmarkImage = async (res, req) => {
+	const bookMark = await Upload.findOne({
+		where: { user_id: req.body.user_id, img_id: req.body.img_id },
+	});
+	if (!Object.keys(bookMark)) {
+		try {
+			const create = await Upload.create(req.body);
+			return res.status(200).json({ "message": "bookmarked added" });
+		} catch (error) {
+			console.log(error);
+			return res.status(500).json({ "message": error });
+		}
+	}
+	await Upload.destroy({ where: { id: req.body.id } });
+	return res.status(200).json({ "message": "bookmarked removed" });
 };
 
 export default downloadImage;
