359e6765eb3cee6b58096fdd12bf5ff4ca84fc63
Afowowe Olumide Victor <volumide42@gmail.com>
2022-11-15 22:47:16 +0100

    add compression file

--- index.js
+++ index.js
@@ -2,8 +2,11 @@ import express, { json } from "express";
 import cors from "cors";
 import connect from "./src/config/db.config.js";
 import routes from "./src/config/routes.js";
+import compression from "compression";
 const PORT = 5000;
 const app = express();
+app.use(compression);
+
 app.use(json());
 app.use(cors({ origin: "*" }));
 
