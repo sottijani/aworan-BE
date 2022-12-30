cc53f897a0ba7225c7834c65774da776c2873682
Afowowe Olumide Victor <volumide42@gmail.com>
2022-12-30 23:10:01 +0100

    chnage port

--- index.js
+++ index.js
@@ -1,16 +1,16 @@
-import express, { json } from "express";
-import cors from "cors";
-import connect from "./src/config/db.config.js";
-import routes from "./src/config/routes.js";
-import compression from "compression";
-const PORT = 5000;
-const app = express();
-app.use(compression);
+import express, { json } from "express"
+import cors from "cors"
+import connect from "./src/config/db.config.js"
+import routes from "./src/config/routes.js"
+import compression from "compression"
+const PORT = 4000
+const app = express()
+app.use(compression)
 
-app.use(json());
-app.use(cors({ origin: "*" }));
+app.use(json())
+app.use(cors({ origin: "*" }))
 
-connect.sync();
-routes(app);
+connect.sync()
+routes(app)
 
-app.listen(PORT, () => console.log("app is listening to port " + PORT));
+app.listen(PORT, () => console.log("app is listening to port " + PORT))
