221de2c1a612ff156224074d997524fe3f2a1a73
Afowowe Olumide Victor <volumide42@gmail.com>
2022-12-31 09:21:15 +0100

    revert port

--- index.js
+++ index.js
@@ -2,10 +2,10 @@ import express, { json } from "express"
 import cors from "cors"
 import connect from "./src/config/db.config.js"
 import routes from "./src/config/routes.js"
-// import compression from "compression"
-const PORT = 9000
+// import compression from "compression";
+const PORT = 5000
 const app = express()
-// app.use(compression)
+// app.use(compression);
 
 app.use(json())
 app.use(cors({ origin: "*" }))
